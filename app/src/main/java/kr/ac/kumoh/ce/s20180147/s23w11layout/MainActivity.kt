cb4e9cd478912c66111425175f6984ac92e0e976
ZeppIine <kim0904min@gmail.com>
2023-11-13 15:48:59 +0900

    add fun MyLinearLayout

--- app/src/main/java/kr/ac/kumoh/ce/s20180147/s23w11layout/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180147/s23w11layout/MainActivity.kt
@@ -3,12 +3,18 @@ package kr.ac.kumoh.ce.s20180147.s23w11layout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import kr.ac.kumoh.ce.s20180147.s23w11layout.ui.theme.S23W11LayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,8 +34,28 @@ fun MyApp(){
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            Greeting("프로그래밍")
+//            Greeting("프로그래밍")
+            MyLinearLayout()
         }
+    }
+}
+
+@Composable
+fun MyLinearLayout(){
+    Column{
+        Row{
+            Text("안녕하세요",
+                Modifier.background(Color.Yellow)
+                    .padding(8.dp)
+            )
+            Text(text = "컴공",
+                modifier = Modifier.background(Color.Cyan)
+                    .padding(8.dp)
+            )}
+        Text(text = "어디서 본 것 같은 예제",
+            modifier = Modifier.background(Color.Magenta)
+                .padding(8.dp)
+        )
     }
 }
 
